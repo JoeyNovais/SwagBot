01a17b5db41c8dcf23a4303e8d215488037eeeae
Joey Novais <novaisjoey@gmail.com>
2017-05-30 18:01:24 +0200

    reconnaissance des liens youtube

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var Discord = require("discord.js");
 var client = new Discord.Client();
+var musicChannel;
+var regExp =
+    new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})");
 
 client.on('ready', () => {
+    musicChannel = client.channels.find('name', 'musique');
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (message.channel.name === musicChannel.name && !message.author.bot) {
+        try {
+            if (regExp.exec(message.content)) {
+                if (!message.content.includes("youtube")) {
+                    musicChannel.send("Sorry, I can only take links from Youtube for now :( Blame Grokon for this !")
+                }
+                musicChannel.send('this is a link.');
+            }
+        } catch (e) {
+            musicChannel.send("Sorry, I couldn't recognize this as a link :( Blame Grokon for this !");
+        }
     }
 });
 
